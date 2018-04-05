1f2d3487af3d2394d6070bf29e13c8bf2172a4d2
Diego <Diego@192.168.1.33>
2018-04-05 13:28:43 -0500

    Fix entities Ingrdient and UnitOfMeasure

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -15,7 +15,7 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
     @ManyToOne
